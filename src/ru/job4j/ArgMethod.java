d1d829b2698952901346ef65974758c8263d69af
SenyuginPavel <p.senyugin@yandex.ru>
2021-08-14 14:18:57 +0300

    4.1. Аргументы [#295521]

---
+++ src/ru/job4j/ArgMethod.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class ArgMethod {
+    public static void name(String name){
+        System.out.println("hallo, " + name);
+    }
+    public static void main(String[] args){
+      String name = "job4j";
+        ArgMethod.name(name);
+        ArgMethod.name(name);
+        ArgMethod.name(name);
+        ArgMethod.name(name);
+    }
+}
