5f81ecf9008ef720ab224e3a2772d9a9d0410798
pseny <p.senyugin@yandex.ru>
2021-10-25 10:48:42 +0300

    4.6.1. Switch [#334070]

---
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class MultipleSwitchWeek {
+    public static int numberOfDay(String name) {
+        return switch (name) {
+            case "Понедельник", "Monday" -> 1;
+            case "1" -> 1;
+            default -> -1;
+        };
+    }
+}
